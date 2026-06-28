6f807eb73bb206ea9fd00378fabc44e9c29c4801
Ajay Chambers <W3Dojo@Gmail.com>
2026-06-28 02:45:03 -0700

    Removed unnecessary comments & settings from Windows-x86_64 toolchain

--- cmake/toolchain/Windows-x86_64.cmake
+++ cmake/toolchain/Windows-x86_64.cmake
@@ -1,17 +1,8 @@
-# =========================================================
-# Windows 11 (x86_64) Toolchain — GNU-style clang-cl flags
-# =========================================================
-CMAKE_MINIMUM_REQUIRED(VERSION 4.3.2)
-
-set(CMAKE_CXX_STANDARD 23)
-set(CMAKE_CXX_STANDARD_REQUIRED ON)
-set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
-
-
+# System/Architecture
 set(CMAKE_SYSTEM_NAME Windows)
 set(CMAKE_SYSTEM_PROCESSOR x86_64)
 
-# Compiler
+# C/C++ Compilers
 set(CMAKE_C_COMPILER clang)
 set(CMAKE_CXX_COMPILER clang++)
 
